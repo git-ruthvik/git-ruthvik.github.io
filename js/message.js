16faf7f3e077d2e5a161d64b7ae2f5d7892fe76f
Ruthvik Rao <ruthvik.b.r@gmail.com>
2021-06-11 23:58:37 +0530

    Added welcome pop-up window

---
+++ js/message.js
@@ -0,0 +1,4 @@
+function message() {
+    swal("Hi There!","This page is going to be completely re-designed, work is under progress and will be pushing it at the end of this month! Yeh, I set deadlines to get work done, self motivation!");
+
+}
