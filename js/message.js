cf1df5b005328dde7da6aa149e4cc3eb83d13588
Ruthvik Rao <49203819+git-ruthvik@users.noreply.github.com>
2021-06-20 01:53:03 +0530

    text update

--- js/message.js
+++ js/message.js
@@ -1,4 +1,4 @@
 function message() {
-    swal("Hi There!","This page is going to be completely re-designed, work is under progress and will be pushing it at the end of this month! Yeh, I set deadlines to get work done, self motivation!");
+    swal("Hi There!","This page is going to be completely re-designed, work is under progress and will be pushing it at the end of this month. Yeh, I set deadlines to get work done, self motivation! Feel free to look around here");
 
 }
